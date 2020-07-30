9485db921ac8933078084cab182e675af0d8766b
danush-95 <danushkumarv@gmail.com>
2020-07-30 18:41:29 +0200

    added ray shere test

--- tests/test_intersect.py
+++ tests/test_intersect.py
@@ -1,15 +1,16 @@
 from pygeo.intersect import (
-    intersect,
+    
     _intersect_ray_with_sphere,
-    _intersect_ray_with_triangle,
+    
 )
 from pygeo.objects import Ray, Sphere, Triangle
 
-
-# intersect
-
-
 # _intersect_ray_with_sphere
+def test_given_ray_sphere_same_origin_return_one_point_intersection():
+    assert (_intersect_ray_and_sphere(Ray((0,0,0),(0,-1,0)),Sphere((0,0,0),5)) == Point((0,-5,0))) is True
 
+def test_given_ray_sphere_no_intersection_return_false():
+    assert (_intersect_ray_and_sphere(Ray((0,0,0),(0,1,0)),Sphere((5,5,5),5)) is False) is True
 
-# _intersect_ray_with_triangle
+def test_given_ray_sphere_return_tangent_intersection():
+    assert (_intersect_ray_and_sphere(Ray((0,0,0),(1,1,0)),Sphere((1,1,1),1)) == Point((1,1,0))) is True
